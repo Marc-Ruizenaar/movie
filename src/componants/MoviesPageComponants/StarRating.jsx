4f36a6018e6cdf9a1d0883e210d5539c75975e58
Marc-Ruizenaar <info@marcruizenaar.com>
2025-01-24 16:15:13 +0100

    Check

--- src/componants/MoviesPageComponants/StarRating.jsx
+++ src/componants/MoviesPageComponants/StarRating.jsx
@@ -1,28 +1,24 @@
 import React from "react";
 import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
 
-export default function StarRating({ rating, title }) {
+export default function StarRating({ rating }) {
   const fullStars = Math.floor(rating / 2);
   const halfStar = rating % 2 >= 1 ? 1 : 0;
   const emptyStars = 5 - fullStars - halfStar;
 
   return (
     <div className="star-rating-container">
-      {title ? <h3>{title}</h3> : ""}
-
-      <div className="star-rating">
-        <div>
-          {[...Array(fullStars)].map((_, index) => (
-            <FaStar key={index} />
-          ))}
-          {halfStar === 1 && <FaStarHalfAlt />}
-          {[...Array(emptyStars)].map((_, index) => (
-            <FaRegStar key={index} />
-          ))}
-        </div>
-
-        <p className="rating-number">{(rating / 2).toFixed(1)}</p>
-      </div>
+    <h3>IMDb</h3>
+    <div className="star-rating">
+      {[...Array(fullStars)].map((_, index) => (
+        <FaStar key={index} />
+      ))}
+      {halfStar === 1 && <FaStarHalfAlt />}
+      {[...Array(emptyStars)].map((_, index) => (
+        <FaRegStar key={index} />
+      ))}
+      <span className="rating-number">{(rating / 2).toFixed(1)}</span>
+    </div>
     </div>
   );
 }
