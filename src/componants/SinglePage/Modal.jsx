d4e8c30865f079efdb49f7c49435fe1b5a85c0d3
odai-dh <odai.dahi@hyperisland.se>
2025-01-27 10:37:25 +0100

    styling changes on single movie page

--- src/componants/SinglePage/Modal.jsx
+++ src/componants/SinglePage/Modal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../css/modal.css";
+import { IoMdClose } from "react-icons/io";
 
 export default function Modal({ show, onClose, children }) {
   if (!show) {
@@ -10,7 +11,7 @@ export default function Modal({ show, onClose, children }) {
     <div className="modal-overlay">
       <div className="modal-content">
         <button className="modal-close" onClick={onClose}>
-          &times;
+          <IoMdClose size={30} />
         </button>
         {children}
       </div>
