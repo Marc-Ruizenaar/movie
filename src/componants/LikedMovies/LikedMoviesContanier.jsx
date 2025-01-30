634bbe47b4ff6521ae59e2b61515a2e568d94968
odai-dh <odai.dahi@hyperisland.se>
2025-01-30 17:26:02 +0100

    fixing the layout of the liked movie page

---
+++ src/componants/LikedMovies/LikedMoviesContanier.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import LikedMoviesList from "./LikedMoviesList";
+import Header from "../UIComponant/Header/Header";
+import Footer from "../UIComponant/Footer/Footer";
+import Community from "../HomePage/Community/Community";
+import "../../css/likedMoviePage.css";
+
+
+
+
+export default function LikedMoviesContaner() {
+  return (
+    <main>
+      <div className="liked-movies-page container">
+        <Header />
+        <LikedMoviesList />
+        
+      </div>
+        <div className="footer-sec">
+      <Community />
+
+        <Footer />  
+        </div>
+    </main>
+  );
+}
--- src/pages/LikedMoviePage.jsx
+++ src/pages/LikedMoviePage.jsx
@@ -1,21 +1,10 @@
-import "../css/likedMoviePage.css";
-import LikedMoviesList from "../componants/LikedMovies/LikedMoviesList";
-import Header from "../componants/UIComponant/Header/Header";
-import Footer from "../componants/UIComponant/Footer/Footer";
-import Community from "../componants/HomePage/Community/Community";
+import LikedMoviesContaner from "../componants/LikedMovies/LikedMoviesContanier";
 
 export default function LikedMoviesPage() {
 
   return (
     <main>
-      <div className="liked-movies-page container">
-        <Header />
-        <div className="content liked-movies-page">
-          <LikedMoviesList />
-        </div>
-        <Community />
-      </div>
-      <Footer />
+        <LikedMoviesContaner />
     </main>
   );
 }
