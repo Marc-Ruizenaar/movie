bc026fbe3021c1211756004bd01c06f71e637ff6
Marc-Ruizenaar <info@marcruizenaar.com>
2025-02-04 15:07:58 +0100

    Removed unused link

--- src/componants/LikedMovies/LikedMoviesList.jsx
+++ src/componants/LikedMovies/LikedMoviesList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLikedMovies } from "./LikedMoviesContext";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../../css/likedMoviePage.css";
 
 export default function LikedMoviesList() {
