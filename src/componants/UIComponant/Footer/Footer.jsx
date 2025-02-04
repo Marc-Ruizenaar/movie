4dac3e2d16e228fa945822caef98261cf2ac30cb
Marc-Ruizenaar <info@marcruizenaar.com>
2025-02-04 15:19:57 +0100

    Updated packages and footer esling warnings

--- src/componants/UIComponant/Footer/Footer.jsx
+++ src/componants/UIComponant/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const SubContainer = ({ title, links }) => {
       <ul>
         {links.map((link, index) => (
           <li key={index}>
-            <a href="#" aria-label={link}>{link}</a>
+            <a href="/" aria-label={link}>{link}</a>
           </li>
         ))}
       </ul>
@@ -43,9 +43,9 @@ export default function Footer() {
       <div className="copyrightContainer">
         <div className="copyright">@2025 StreamVibe, All Rights Reserved</div>
         <div className="policyContainer">
-          <a href="#" className="policyLink" aria-label="Terms of Use">Terms of Use</a>
-          <a href="#" className="policyLink" aria-label="Privacy Policy">Privacy Policy</a>
-          <a href="#" className="policyLink" aria-label="Cookie Policy">Cookie Policy</a>
+          <a href="/" className="policyLink" aria-label="Terms of Use">Terms of Use</a>
+          <a href="/" className="policyLink" aria-label="Privacy Policy">Privacy Policy</a>
+          <a href="/" className="policyLink" aria-label="Cookie Policy">Cookie Policy</a>
         </div>
       </div>
     </footer>
